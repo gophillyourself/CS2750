ac45caba09d6a03be60d8c847d1b880135e92ac6
Phill Janowski <phill.janowski@nisc.coop>
2019-02-21 19:09:07 -0600

    finished factor.sh

--- project1/factor.sh
+++ project1/factor.sh
@@ -5,22 +5,42 @@
 # finds the smallest factor of a number inputted as the argument
 
 usage() {
-    echo "usage: " $0 " requires the following argument"
-    echo "-n|--number : number to find the smallest prime factor of"
+    echo "usage: " $0 " requires the following argument with the following stipulations"
+    echo "-n|--number : number to find the smallest prime factor of, must be between 10 and 100"
     echo "[-h|--help] : display this message"
 
 }
-if [[ "$#" != 2 ]] || [[ "$1" = "-h" ]] || [[ "$1" = "--help" ]] || [[ "$1" != "-n" ]] || [[ "$1" != "--number" ]]; then
+
+# checking for correct amount of args, for the help arg, and that the number arg is there
+if [[ "$#" != 2 ]] || [[ "$1" = "-h" ]] || [[ "$1" = "--help" ]] || [[ "$1" != "-n" ]] && [[ "$1" != "--number" ]]; then
     usage
     exit 1
 fi
 
 # getting the number into the $1 slot
 shift
+#checking number is in the right range
+if [[ $1 -gt 100 ]] || [[ $1 -lt 10 ]]; then
+    usage
+    exit 1
+fi
 
-lowestNumber = expr "$1" / 2
-moddedNumer = $(("$1" % "$lowestNumber"))
-while [[ "lowestNumber"%  ]]
+#initializing things for while loop
+lowestNumber=$(($1 / 2))
+currentNumber=$lowestNumber
+moddedNumber=$(($1 % $lowestNumber))
+while [[ "$currentNumber" -gt 1 ]] 
 do
-
+    # checking for perfect divisibility for every number 
+    moddedNumber=$(($1 % $currentNumber))
+    if [[ $moddedNumber = 0 ]]; then
+        lowestNumber=$currentNumber
+    fi
+    currentNumber=$(( $currentNumber - 1 ))
 done
+# checking that the lowest number isn't the name as the one we calculated in the initialization
+if [[ $lowestNumber = $(($1 / 2)) ]]; then 
+    echo "This number is prime"
+else
+    echo $lowestNumber
+fi
