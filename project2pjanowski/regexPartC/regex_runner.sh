2f7f45e6af18c2198ecdacc1afcaadb751d6fe0d
Phill Janowski <phill.janowski@nisc.coop>
2019-03-19 18:25:04 -0500

    finished project2

--- project2pjanowski/regexPartC/regex_runner.sh
+++ project2pjanowski/regexPartC/regex_runner.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-
+# Phillip Janowski
+# CS2750
+# regex_runner.sh
+# runs the list of regexes in a file 'regex' against text in the file 'input' with sed
 regexFile='regex'
 
 inputFile='input'
 
 while read -r regex; do
-	echo "---------------"
-	echo "$regex"
-	echo sed "$regex" "$inputFile"
-	sed "$regex" "$inputFile"
+	if [[ $(echo "$regex" | sed '/^#/g') ]]; then
+			echo "---------------"
+			echo "$regex"
+			echo sed "$regex" "$inputFile"
+			sed "$regex" "$inputFile"
+	fi
+
 
 done < "$regexFile"
