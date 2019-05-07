e0ceebb1e23bd4abf17098107218d16ac117ee34
phill janowski <philajan@gmail.com>
2019-05-06 21:26:27 -0500

    more work on project 5

--- project5pjanowski/master.c
+++ project5pjanowski/master.c
@@ -6,13 +6,17 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include<sys/wait.h> 
 
 #define SHMKEY 1392
 #define BUFF_SZ sizeof ( int )
 
 int main (int argc, char * argv[]) {
 	char usage[] = "Usage: master [-h | [-n number]]\n-h | displays this message\n-n | number to share with worker, defaults to 100";
-	int number;
+	int * number;
+	char * numString;
 	int o;
 	while(( o = getopt (argc, argv, "n:h:")) != -1) {
 		switch (o) {
@@ -21,6 +25,7 @@ int main (int argc, char * argv[]) {
 				exit (0);
 			
 			case 'n':
+				numString = optarg;
 				number = atoi(optarg);
 				break;
 		}	
@@ -29,23 +34,43 @@ int main (int argc, char * argv[]) {
 		number = 100;
 	}	
 	printf("%d\n", number);
-	int * shmid = shmget ( SHMKEY, BUFF_SZ, 0777 | IPC_CREAT );
-	shmid = 0;
+	int shmid = shmget ( SHMKEY, BUFF_SZ, 0777 | IPC_CREAT );
 	if (shmid == -1) {
-		cerr << "Error in shmget\n" << endl;
+		fprintf(stderr, "Error in shmget\n");
 		exit (1);
 	}
 
 	int * shint = ( int * ) (shmat (shmid, 0, 0));
-	shmdt(shint);
-	shmctl(shmid, IPC_RMID, NULL);
+
 	int pid = fork();
+	printf("%d\n", pid);
 	// child
 	if (pid == 0) {
-
+		char * execArg[] = { "./worker", "-n", numString};
+		int execed = execvp(execArg[0], execArg);
+		if(execed == -1) {
+			perror("execvp failed\n");
+			exit(1);
+		}
 	} else {
-		execvp("worker", "-n", itoa(number));
+		printf("about to wait\n");
+		printf("shint = %d\n", shint);
+		printf("number = %d\n", number);
+		
+		wait(NULL);
+		if( shint != number) {
+			printf("the shared memory copy didn't work\n");
+			printf("shint = %d\n", shint);
+			printf("number = %d\n", number);
+		}
+		
+		printf("done\n");
+
+		printf("Inputted number = %d\n", shint);
+		shint = 0;
+		
+		shmdt(shint);
+		shmctl(shmid, IPC_RMID, NULL);
 	}
-	getpid
 	return 0;
 }
--- project5pjanowski/worker.c
+++ project5pjanowski/worker.c
@@ -6,11 +6,38 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include<sys/wait.h> 
 
 #define SHMKEY 1392
 #define BUFF_SZ sizeof ( int )
 
 int main (int argc, char * argv[]) { 
 	
+	char usage[] = "Usage: master [-h | [-n number]]\n-h | displays this message\n-n | number to share with worker, defaults to 100";
+	int * number;
+	int o;
+	while(( o = getopt (argc, argv, "n:h:")) != -1) {
+		switch (o) {
+			case 'h':
+				printf("%s\n", usage);
+				exit (0);
+			
+			case 'n':
+				number = atoi(optarg);
+				break;
+		}	
+	}
+
+	int shmid = shmget ( SHMKEY, BUFF_SZ, 0777 | IPC_CREAT );
+	if (shmid == -1) {
+		fprintf(stderr, "Error in shmget\n");
+		exit (1);
+	}
+
+	int * shint = ( int * ) (shmat (shmid, 0, 0));
+	shint = number;
+	printf("hi from the worker shint = %d\n", shint);
 	return 0;	
 }
