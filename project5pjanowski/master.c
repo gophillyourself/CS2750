4ac2443dd5a622d11fe6c0a1c8f65c99e72743e4
phill janowski <philajan@gmail.com>
2019-05-08 22:22:53 -0500

    project 5 finished

--- project5pjanowski/master.c
+++ project5pjanowski/master.c
@@ -16,10 +16,13 @@
 
 int main (int argc, char * argv[]) {
 	char usage[] = "Usage: master [-h | [-n number]]\n-h | displays this message\n-n | number to share with worker, defaults to 100";
-	int * number;
+	int number;
 	char * numString;
 	int o;
-	printf("starting\n");
+	if (argc < 1) {
+		printf("%s\n", usage);
+		exit(1);
+	}
 	while(( o = getopt (argc, argv, "n:h:")) != -1) {
 		switch (o) {
 			case 'h':
@@ -38,43 +41,35 @@ int main (int argc, char * argv[]) {
 		numString = (char*) malloc(sizeof("100") + 1);
 		numString = "100\0";
 	}	
-	printf("%d\n", number);
     int shmid = shmget ( SHMKEY, BUFF_SZ, 0666 | IPC_CREAT );
-
+	int * pint = (int * ) (shmat (shmid, 0, 0));
+	if (shmid == -1) {
+		fprintf(stderr, "[master] Error in shmget\n");
+		exit (1);
+	}
     int pid = fork();
-    printf("%d\n", pid);
 	// child
 	if (pid == 0) {
 		char * execArg[] = { "./worker", "-n", numString, NULL };
 
 		int execed = execvp(execArg[0], execArg);
 		if(execed == -1) {
-			perror("execvp failed\n");
+			perror("[master] execvp failed\n");
 			exit(1);
 		}
 	} else {
-        printf("[master] shmid = %d\n", shmid);
-        if (shmid == -1) {
-            fprintf(stderr, "Error in shmget\n");
-            exit (1);
-        }
-
-        int * pint = (int * ) (shmat (shmid, 0, 0));
-		printf("about to wait\n");
-		printf("pint = %d\n", pint);
-		printf("number = %d\n", number);
-		
-		wait(NULL);
-		if( pint != number) {
-			printf("the shared memory copy didn't work\n");
-			printf("pint = %d\n", pint);
-			printf("number = %d\n", number);
+		*pint = 0;
+		int count = 0;
+		while (*pint == 0 || count >= 10) {
+			sleep(1);
+			count++;
 		}
-		
-		printf("done\n");
-
-		printf("Inputted number = %d\n", pint);
-
+		if( *pint == 0) {
+			printf("[master] The shared memory didn't get a value other than 0.\n");
+		} else {
+			printf("[master] Recieved %d in the shared memory\n", *pint);
+		}
+		*pint = 0;
 		shmdt(pint);
 		shmctl(shmid, IPC_RMID, NULL);
 	}
--- project5pjanowski/worker.c
+++ project5pjanowski/worker.c
@@ -13,13 +13,16 @@
 #define SHMKEY 1392
 #define BUFF_SZ sizeof ( int )
 
+int greatestPrimeNum(int number);
+
 int main (int argc, char * argv[]) { 
 	
 	char usage[] = "Usage: worker [-h | [-n number]]\n-h | displays this message\n-n | number to share with worker, defaults to 100";
-	int * number;
+	int number;
 	int o;
-	for (int i = 0; i < argc; i++) {
-		printf("[worker] arg %d = %s\n", i, argv[i]);
+	if (argc < 1) {
+		printf("%s\n", usage);
+		exit(1);
 	}
 	
 	while(( o = getopt (argc, argv, "n:h:")) != -1) {
@@ -34,18 +37,61 @@ int main (int argc, char * argv[]) {
 		}	
 	}
 
-	int shmid = shmget ( SHMKEY, BUFF_SZ, 0666 );
-    printf("[worker] shmid = %d\n", shmid);
+	int shmid = shmget ( SHMKEY, BUFF_SZ, 0666 | IPC_CREAT);
     if (shmid == -1) {
-		fprintf(stderr, "Error in shmget\n");
+		fprintf(stderr, "[worker] Error in shmget\n");
 		exit (1);
 	}
 
 	int * cint = (int * ) (shmat (shmid, 0, 0));
-    printf("[worker] cint = %d\n", cint);
-	printf("[worker] before cint = %d\n", cint);
-    cint = number;
-	printf("[worker] after cint = %d\n", cint);
+	if (*cint != 0) {
+		printf("[worker] 0 was not found in the shared memory\n Exiting\n");
+		exit(1);
+	} else {
+		printf("[worker] 0 was found in the shared memory\n Continuing\n");
+	}
+	int greatest = greatestPrimeNum(number);
+	*cint = greatest;
+	int count = 0;
+	while (*cint != 0 || count >= 10) {
+		sleep(1);
+		count++;
+	}
+	if (*cint == 0) {
+		printf("[worker] Shared memory has been set back to 0\n");
+	} else {
+		printf("[worker] Shared memory was never set back to 0\n");
+	}
     shmdt(cint);
     return 0;
+}
+
+int greatestPrimeNum(int number) {
+	int greatest = number;
+	int tempNum = number;	
+	int isPrime = 1;
+	printf("greatest Prime\n");
+	while (greatest > 1) {
+		isPrime = 0;
+		printf("greatest = %d\n", greatest);
+
+		if (greatest % 2 != 0) {
+			int dividend = 3;
+			while (dividend <= greatest) {
+				if (dividend == greatest) {
+					return greatest;
+				}
+				if (greatest % dividend == 0) {
+					isPrime = 0;
+					break;
+				}
+				dividend++;
+			}
+		}
+		if (isPrime == 1) {
+			break;
+		}
+		greatest = greatest - 1;
+	}
+	return -1;
 }
